32d5a3e423783069a9424a307daa8b475d103b9e
wrosoff4 <thadeus.rosoff@gmail.com>
2021-10-16 00:06:00 -0400

    took secret key out of env var to keep the demo runnable

--- flask_blog/config.py
+++ flask_blog/config.py
@@ -4,7 +4,7 @@ base_dir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SK')
+    SECRET_KEY = os.environ.get('6de1bc827fb03e7be8ac70c3bd060f7b')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(base_dir, 'site.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     MAIL_SERVER = 'smtp.googlemail.com'
